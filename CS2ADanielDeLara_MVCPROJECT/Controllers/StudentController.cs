a8bbba30072950e8778632e74c09222695037d55
14thDream <dndelara@proton.me>
2025-03-08 11:37:56 +0800

    Added a view of student details

--- CS2ADanielDeLara_MVCPROJECT/Controllers/StudentController.cs
+++ CS2ADanielDeLara_MVCPROJECT/Controllers/StudentController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CS2ADanielDeLara_MVCPROJECT.BusLogic.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CS2ADanielDeLara_MVCPROJECT.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly StudentService _studentService = new();
         public IActionResult Index()
         {
-            return View();
+            var students = _studentService.GetAll();
+            return View(students);
         }
     }
 }
