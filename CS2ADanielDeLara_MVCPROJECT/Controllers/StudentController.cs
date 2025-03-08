fd31de74b47090b9c649033ec0df3cb03cdd9b55
14thDream <dndelara@proton.me>
2025-03-08 10:54:08 +0800

    Add Student Controller

---
+++ CS2ADanielDeLara_MVCPROJECT/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CS2ADanielDeLara_MVCPROJECT.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
