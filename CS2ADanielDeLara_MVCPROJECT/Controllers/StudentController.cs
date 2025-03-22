c2032348064358f459dfcad28dad810c36e67e32
14thDream <dndelara@proton.me>
2025-03-22 10:58:24 +0800

    Added logger to Student Controller.

--- CS2ADanielDeLara_MVCPROJECT/Controllers/StudentController.cs
+++ CS2ADanielDeLara_MVCPROJECT/Controllers/StudentController.cs
@@ -1,11 +1,20 @@
-﻿using CS2ADanielDeLara_MVCPROJECT.BusLogic.Service;
+﻿using CS2ADanielDeLara_MVCPROJECT.BusLogic.Model;
+using CS2ADanielDeLara_MVCPROJECT.BusLogic.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CS2ADanielDeLara_MVCPROJECT.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ILogger<StudentController> _logger;
         private readonly StudentService _studentService = new();
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+            _studentService = new StudentService();
+        }
+
         public IActionResult Index()
         {
             var students = _studentService.GetAll();
@@ -16,5 +25,7 @@ namespace CS2ADanielDeLara_MVCPROJECT.Controllers
         {
             return View();
         }
+
+
     }
 }
